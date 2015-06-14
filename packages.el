5b7247c4f41d3cd512295e37b076ea6456b3a19e
Eivind Fonn <evfonn@gmail.com>
2015-06-14 18:25:33 +0200

    Further improvements
    - Disable elpy's flymake and use flycheck instead
    - Enable company and yasnippet dependent on configuration
    - Diminish Elpy
    - Add completion to inferior python mode

--- packages.el
+++ packages.el
@@ -17,20 +17,39 @@
       '(
         company
         elpy
+        flycheck
         ))
 
 (setq elpy-excluded-packages '())
 
 (defun elpy/init-elpy ()
   (use-package elpy
+    :diminish elpy-mode
     :config
+
     ;; Elpy removes the modeline lighters. Let's override this
     (defun elpy-modules-remove-modeline-lighter (mode-name))
+
+    (setq elpy-modules '(elpy-module-sane-defaults
+                         elpy-module-eldoc
+                         elpy-module-pyvenv))
+
+    (when (configuration-layer/layer-usedp 'auto-completion)
+      (add-to-list 'elpy-modules 'elpy-module-company)
+      (add-to-list 'elpy-modules 'elpy-module-yasnippet))
+
     (elpy-enable)
     ))
 
-(when (configuration-layer/layer-usedp 'auto-completion)
-  (defun elpy/post-init-company ()
-    (spacemacs|add-company-hook python-mode)
-    )
+(defun elpy/post-init-company ()
+  (spacemacs|add-company-hook inferior-python-mode)
+  (push 'company-capf company-backends-inferior-python-mode)
+  (add-hook 'inferior-python-mode-hook
+            (lambda ()
+              (setq-local company-minimum-prefix-length 0)
+              (setq-local company-idle-delay 0.5)))
+  )
+
+(defun elpy/post-init-flycheck ()
+  (add-hook 'elpy-mode-hook 'flycheck-mode)
   )
